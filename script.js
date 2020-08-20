f7ef577a98047e7c1d54f125d6f05a7977e5a9d4
alesin <alesintipler@gmail.com>
2020-08-19 21:40:10 -0400

    feat: playRound game function

--- script.js
+++ script.js
@@ -8,4 +8,49 @@ function computerPlay(){
     if(random === 3) return 'scissors'
 }
 
-console.log(computerPlay())
+// console.log(computerPlay())
+
+function playRound(playerChoice, computerChoice) {
+    let winner
+    playerChoice = playerChoice.toLowerCase()
+  
+    //check for tie
+    if(playerChoice === computerChoice){
+      // Pit two computers against each other... or try your luck against the machine!
+      // IN OTHER WORDS  ===  only uncomment 1 of the 2 lines below
+      playerChoice = computerPlay()
+      // playerChoice = prompt('Tie, pick again! Rock, paper or scissors?')
+      let newComputerChoice = computerPlay()
+      playRound(playerChoice, newComputerChoice)
+    }
+  
+    //check for rock
+    if(playerChoice === 'rock') {
+//       if(computerChoice === 'paper') computerPoints++
+//       else playerPoints++
+      if(computerChoice === 'paper') winner = 'computer'
+      else winner = 'player'
+    }
+  
+    //check for paper
+    if(playerChoice === 'paper') {
+      if(computerChoice === 'scissors') winner = 'computer'
+      else winner = 'player'
+    } 
+  
+    //check for scissors
+    if(playerChoice === 'scissors') {
+      if(computerChoice === 'rock') winner = 'computer'
+      else winner = 'player'
+    }
+  
+    // console.log(winner)
+    if (winner === 'computer') return [winner, `You lose! ${computerChoice} beats ${playerChoice}!`]
+    else return [winner, `You win! ${playerChoice} beats ${computerChoice}!`]
+  
+}
+
+// *****    TESTING the 1st TWO functions    before move to the final GAME stage *****
+let playerSelect = 'rock'
+let computerSelect = computerPlay() 
+console.log(playRound(playerSelect, computerSelect))
